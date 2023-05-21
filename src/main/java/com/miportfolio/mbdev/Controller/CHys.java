343dd09e8f6aaa5a6638a002f3190d22e3b192f4
MBareiro <58647378+MBareiro@users.noreply.github.com>
2023-05-21 19:35:11 -0300

    prueba 2

--- src/main/java/com/miportfolio/mbdev/Controller/CHys.java
+++ src/main/java/com/miportfolio/mbdev/Controller/CHys.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://mbdevfrontend.web.app/"})
+@CrossOrigin(origins = {"https://mbdevfrontend.web.app/","http://localhost:4200"})
 @RequestMapping("/skill")
 public class CHys {
 
