1eca616a23184610d0912ed5ff6b4e51b094c4c2
MBareiro <58647378+MBareiro@users.noreply.github.com>
2023-05-28 21:12:10 -0300

    Prueba3

--- src/main/java/com/miportfolio/mbdev/Repository/Rhys.java
+++ src/main/java/com/miportfolio/mbdev/Repository/Rhys.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.miportfolio.mbdev.Repository;
 
 import com.miportfolio.mbdev.Entity.hys;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-/**
- *
- * @author Usuario
- */
+@Repository
 public interface Rhys extends JpaRepository<hys, Integer>{
     Optional<hys> findByNombre(String nombre);
     public boolean existsByNombre(String nombre);
